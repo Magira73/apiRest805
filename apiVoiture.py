10caf3ca59bb0d9be476d2442e7a24cb946cec8f
Magira73 <60464255+Magira73@users.noreply.github.com>
2022-02-28 20:54:49 +0100

    Update apiVoiture.py

--- apiVoiture.py
+++ apiVoiture.py
@@ -1,3 +1,4 @@
+from ast import If
 from flask import Flask
 from flask_restful import Resource, Api
 from flask_cors import CORS
@@ -9,11 +10,14 @@ api = Api(app)
 class CalculTime(Resource):
     def get(self, distance, nbArret, tArret):
         time = float(distance)/70.0
-        tmp = tArret*nbArret
+        if int(nbArret) > 0:
+            tmp = int(tArret)*int(nbArret)
+        else:
+            tmp = 0        
         time = time + float(tmp)
         return {'time': time}
 
-api.add_resource(CalculTime, '/calculTime/<distance>/<int:nbArret>/<int:tArret>')
+api.add_resource(CalculTime, '/calculTime/<distance>/<nbArret>/<tArret>')
 
 if __name__ == '__main__':
     app.run(debug=True)
